fc727f901ace7800d87dc5614530eb68f02badfb
1 <a1@1s-MacBook-Pro.local>
2020-12-10 00:44:51 +0200

    add task 2 and answer

--- script.js
+++ script.js
@@ -1,21 +1,32 @@
+// Асинхронность  - это выполнение различных задач одновременно
 const button = document.querySelector('button');
-console.log(button);
 
 const DOMAIN = 'https://api.ipify.org/?format=json';
 async function f(DOMAIN) {
-  button.addEventListener('click', () => {
-    // console.log('hello');
-  });
   let response = await fetch(DOMAIN);
   let info = await response.json();
-  console.log(info);
+  const ip = info.ip;
+
+  const serverUrl = 'http://ip-api.com/';
+  button.addEventListener('click', () => {
+    getPersonalInfo(serverUrl);
+    async function getPersonalInfo() {
+      const url = `${serverUrl}json/${ip}?fields=continent,country,region,city,district`;
+      let response = await fetch(url);
+      let address = await response.json();
+      const ul = document.querySelector('ul');
+
+      function createinfoList(address) {
+        for (let key in address) {
+          if (address[key] !== '') {
+            const li = document.createElement('li');
+            li.textContent = `${key} : ${address[key]}`;
+            ul.appendChild(li);
+          }
+        }
+      }
+      createinfoList(address);
+    }
+  });
 }
 f(DOMAIN);
-
-const serverUrl = 'https://ip-api.com/';
-async function getPersonalInfo(serverUrl) {
-  let response = await fetch(serverUrl);
-  let address = await response.json();
-  console.log(address);
-}
-getPersonalInfo(serverUrl);
